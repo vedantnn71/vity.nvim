cfb1e11314f70216f9bcc7dac6ab56199182d265
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-04-18 15:19:11 +0530

    fix: theme colors fixed to nord colorscheme

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -2,7 +2,6 @@ local M = {}
 
 M.setup = function()
   local lualine = require('lualine')
-  local colors = require('utils.colors')
 
   -- stylua: ignore
 
@@ -26,13 +25,7 @@ M.setup = function()
       -- Disable sections and component separators
       component_separators = '',
       section_separators = '',
-      theme = {
-	-- We are going to use lualine_c an lualine_x as left and
-	-- right section. Both are highlighted by c theme .  So we
-	-- are just setting default looks o statusline
-	normal = { c = { fg = colors.fg, bg = colors.bg } },
-	inactive = { c = { fg = colors.fg, bg = colors.bg } },
-      },
+      theme = 'auto',
     },
     sections = {
       -- these are to remove the defaults
@@ -69,7 +62,6 @@ M.setup = function()
     function()
       return '▊'
     end,
-    color = { fg = colors.blue }, -- Sets highlighting of component
     padding = { left = 0, right = 1 }, -- We don't need space before this
   }
 
@@ -78,39 +70,12 @@ M.setup = function()
     function()
       return ''
     end,
-    color = function()
-      -- auto change color according to neovims mode
-      local mode_color = {
-	n = colors.red,
-	i = colors.green,
-	v = colors.blue,
-	[''] = colors.blue,
-	V = colors.blue,
-	c = colors.magenta,
-	no = colors.red,
-	s = colors.orange,
-	S = colors.orange,
-	[''] = colors.orange,
-	ic = colors.yellow,
-	R = colors.violet,
-	Rv = colors.violet,
-	cv = colors.red,
-	ce = colors.red,
-	r = colors.cyan,
-	rm = colors.cyan,
-	['r?'] = colors.cyan,
-	['!'] = colors.red,
-	t = colors.red,
-      }
-      return { fg = mode_color[vim.fn.mode()] }
-    end,
     padding = { right = 1 },
   }
 
   ins_left {
     'filename',
     cond = conditions.buffer_not_empty,
-    color = { fg = colors.magenta, gui = 'bold' },
   }
 
   ins_left { 'location' }
@@ -120,11 +85,6 @@ M.setup = function()
     'diagnostics',
     sources = { 'nvim_diagnostic' },
     symbols = { error = ' ', warn = ' ', info = ' ' },
-    diagnostics_color = {
-      color_error = { fg = colors.red },
-      color_warn = { fg = colors.yellow },
-      color_info = { fg = colors.cyan },
-    },
   }
 
   -- Insert mid section. You can make any number of sections in neovim :)
@@ -140,31 +100,23 @@ M.setup = function()
     'o:encoding', -- option component same as &encoding in viml
     fmt = string.upper, -- I'm not sure why it's upper case either ;)
     cond = conditions.hide_in_width,
-    color = { fg = colors.green, gui = 'bold' },
   }
 
   ins_right {
     'fileformat',
     fmt = string.upper,
     icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
-    color = { fg = colors.green, gui = 'bold' },
   }
 
   ins_right {
     'branch',
     icon = '',
-    color = { fg = colors.violet, gui = 'bold' },
   }
 
   ins_right {
     'diff',
     -- Is it me or the symbol for modified us really weird
     symbols = { added = ' ', modified = '柳 ', removed = ' ' },
-    diff_color = {
-      added = { fg = colors.green },
-      modified = { fg = colors.orange },
-      removed = { fg = colors.red },
-    },
     cond = conditions.hide_in_width,
   }
 
@@ -172,7 +124,6 @@ M.setup = function()
     function()
       return '▊'
     end,
-    color = { fg = colors.blue },
     padding = { left = 1 },
   }
 
